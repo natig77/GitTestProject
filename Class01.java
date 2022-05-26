510ccebaa5d8ebfc060489e314d31a9406a0a1dd
natig77 <natiggurbanov@gmail.com>
2022-05-26 01:59:35 -0400

    secondcommit

--- Class01.java
+++ Class01.java
@@ -5,6 +5,9 @@ public class Class01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Natig");
+		for(int i=0; i<10; i++) {
+			System.out.print(i);
+		}
 
 	}
 
